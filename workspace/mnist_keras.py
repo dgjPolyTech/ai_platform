240161c5e542ffb0378d42466d57e557fa0b8543
dgjPolyTech <dgjpolytech@gmail.com>
2025-11-21 19:06:03 +0900

    과제용 파일 업로드

---
+++ workspace/mnist_keras.py
@@ -0,0 +1,12 @@
+from tensorflow.keras.datasets import mnist;
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((1000, 28, 28, 1))
+test_images = test_images.astype("float32") / 255
+
+model.compile(optimizer="rmsprop",
+              loss="sparse_categorical_crossentropy",
+              metrics=["accuracy"])
+model.fit(train_images, train_labels, epochs=5, batch_size=64)
